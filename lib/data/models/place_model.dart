ad00cd94583743e36ce964a4820b37eee6955b5a
Amr_BahaaEldin <108246015+AmrBahaaEldin@users.noreply.github.com>
2024-05-01 15:04:16 +0300

    add new widget loading check Network and imporve widget detector

--- lib/data/models/place_model.dart
+++ lib/data/models/place_model.dart
@@ -1,13 +1,13 @@
-class TourStation {
+class TourStationModel {
  late String city;
  late String busNumber;
  late List<String> stations;
  late int price;
-TourStation.fromJson(Map<String,dynamic>json){
+TourStationModel.fromJson(Map<String,dynamic>json){
   city = json['City'];
   busNumber = json['Bus Number'];
   stations = json['Stations'];
   price = json['Price'];
-
-
 }
+}
+
